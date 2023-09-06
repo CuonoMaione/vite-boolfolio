f41f2ef2ec4ee67596d532ea4494951015b4942d
CuonoMaione <cuonomaione99@outlook.it>
2023-09-06 02:11:38 +0200

    api exposure

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppMain from './components/AppMain.vue'
+
+export default{
+
+  name:'App',
+  components:{
+    AppMain   
+  }
+
+}
+</script>
+
+<template>
+
+  <AppMain/>
+</template>
+
+<style lang="scss" >
+@use './styles/general.scss';
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+    <p v-for="post in posts">
+        {{ post.title }}
+       
+    </p>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+
+    export default {
+        name:'AppMain',
+
+        data() {
+            return {
+                posts:[],
+                ApiUrl:'http://127.0.0.1:8000/api/posts',
+            }
+        },
+
+        methods: {
+            getPosts(){
+                axios.get(this.ApiUrl, {
+                    params: {}
+                })
+                .then((response) => {
+                    console.log(response.data.results.data);
+                    this.posts = response.data.results.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                ;  
+
+            }
+        },
+
+        created() {
+            this.getPosts()
+        },
+    }
+</script>
+<style lang="scss">
+   div{
+    text-align: center;
+   }
+</style>
